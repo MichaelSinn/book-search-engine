200b6d750c7e44fb380d3e86ceaaa88233c07e16
MichaelSinn <michaelsinn.contact@gmail.com>
2022-11-19 21:58:49 -0500

    Modified server.js to work with gql rather than express

--- server/server.js
+++ server/server.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const {typeDefs, resolvers} = require('./schemas');
+const {ApolloServer} = require('apollo-server-express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({app});
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`Now listening on localhost: ${PORT}`);
+            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        });
+
+    });
+}
+
+startApolloServer(typeDefs, resolvers);
+
